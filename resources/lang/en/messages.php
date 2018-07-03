5cddbe2eccbd0c2b2a6becdd07ec09d5b7808916
Van Ba Khanh <vanbakhanh@gmail.com>
2018-07-03 23:54:49 +0700

    Update multi language part 5

--- resources/lang/en/messages.php
+++ resources/lang/en/messages.php
@@ -12,8 +12,8 @@ return [
     */
 
     'changed_password' => 'Password has been changed',
-    'create_success' => 'Created successful',
-    'update_success' => 'Updated successful',
+    'created_success' => 'Created successful',
+    'updated_success' => 'Updated successful',
     'deleted_success' => 'Deleted successful',
     'paymented' => 'Thank you for shopping at Nike! Your order has been received and is going through verification process.',
     'verify_email' => 'Check the verify email link that we sent to your email.',
--- resources/lang/vi/messages.php
+++ resources/lang/vi/messages.php
@@ -12,8 +12,8 @@ return [
     */
 
     'changed_password' => 'Mật khẩu đã đổi thành công',
-    'create_success' => 'Thêm mới thành công',
-    'update_success' => 'Cập nhật thành công',
+    'created_success' => 'Thêm mới thành công',
+    'updated_success' => 'Cập nhật thành công',
     'deleted_success' => 'Xóa thành công',
     'paymented' => 'Cảm ơn bạn đã mua sắm ở Nike! Đơn đặt hàng của bạn đã được nhận và đang trải qua quá trình xác minh.',
     'verify_email' => 'Kiểm tra liên kết xác minh trong email mà chúng tôi đã gửi đến email của bạn.',
